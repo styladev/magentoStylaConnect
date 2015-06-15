15a544e6b605d626c2c162988148be59bac7b71d
Patryk Kata <patrykkata@gmail.com>
2015-06-15 11:56:31 +0200

    SC-5: adding comment

--- app/code/community/Styla/Connect/Model/Api2/Converter/Product/Saleable.php
+++ app/code/community/Styla/Connect/Model/Api2/Converter/Product/Saleable.php
@@ -3,7 +3,13 @@ class Styla_Connect_Model_Api2_Converter_Product_Saleable extends Styla_Connect_
 {
     public function runConverter(Varien_Object $dataObject)
     {
-        $dataObject->unsetData('is_salable');
+        /**
+         * Since products are loaded in the context of admin store, and there's no stock for admin -
+         * by default the value of is_salable is set to null and is wrong. To make magento reconsider,
+         * and to allow isSaleable() to work correctly, we need to get rid of this wrong value, first.
+         */
+        $dataObject->unsetData('is_salable');        
+        
         $value = ($dataObject->isSaleable());
         
         $stylaField = $this->getStylaField();
