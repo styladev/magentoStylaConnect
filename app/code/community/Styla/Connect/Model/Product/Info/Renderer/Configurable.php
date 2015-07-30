0aa3654859760129baa8ea561ca3453766a87f10
Patryk Kata <patrykkata@gmail.com>
2015-07-30 15:39:09 +0200

    SC-11: adding the missing price data for options

--- app/code/community/Styla/Connect/Model/Product/Info/Renderer/Configurable.php
+++ app/code/community/Styla/Connect/Model/Product/Info/Renderer/Configurable.php
@@ -1,6 +1,13 @@
 <?php
 class Styla_Connect_Model_Product_Info_Renderer_Configurable extends Styla_Connect_Model_Product_Info_Renderer_Abstract
 {
+    protected $_product;
+    
+    public function getProduct()
+    {
+        return $this->_product;
+    }
+    
     /**
      * Add configurable product's options data to the product info array.
      * This method is basically the same logic that's used for generating the options selects on the product view page.
@@ -18,6 +25,7 @@ class Styla_Connect_Model_Product_Info_Renderer_Configurable extends Styla_Conne
          * the following configurable-data collecting code is a simplified version
          * of the logic used to gather product options on a catalog/product/view details page
          */
+        $this->_product = $product;
         
         //add configurable-specific data.
         $configurableInfo = array();
@@ -67,10 +75,22 @@ class Styla_Connect_Model_Product_Info_Renderer_Configurable extends Styla_Conne
                         $productsIndex = array();
                     }
                     
+                    $product->setConfigurablePrice(
+                        $this->_preparePrice($value['pricing_value'], $value['is_percent'])
+                    );
+                    $product->setParentId(true);
+                    Mage::dispatchEvent(
+                        'catalog_product_type_configurable_price',
+                        array('product' => $product)
+                    );
+                    $configurablePrice = $product->getConfigurablePrice();
+                    
                     $attributeOptions[] = array(
                         'id' => $value['value_index'],
                         'label' => $value['label'],
-                        'products' => $productsIndex   
+                        'products' => $productsIndex,
+                        'price'     => $configurablePrice,
+                        'oldPrice'  => $this->_prepareOldPrice($value['pricing_value'], $value['is_percent']),
                     );
                 }
             }
@@ -84,5 +104,69 @@ class Styla_Connect_Model_Product_Info_Renderer_Configurable extends Styla_Conne
         $productInfo['attributes'] = $configurableInfo;
         
         return $productInfo;
+    }
+    
+    /**
+     * Calculation real price
+     *
+     * @param float $price
+     * @param bool $isPercent
+     * @return mixed
+     */
+    protected function _preparePrice($price, $isPercent = false)
+    {
+        if ($isPercent && !empty($price)) {
+            $price = $this->getProduct()->getFinalPrice() * $price / 100;
+        }
+
+        return $this->_registerJsPrice($this->_convertPrice($price, true));
+    }
+    
+    /**
+     * Calculation price before special price
+     *
+     * @param float $price
+     * @param bool $isPercent
+     * @return mixed
+     */
+    protected function _prepareOldPrice($price, $isPercent = false)
+    {
+        if ($isPercent && !empty($price)) {
+            $price = $this->getProduct()->getPrice() * $price / 100;
+        }
+
+        return $this->_registerJsPrice($this->_convertPrice($price, true));
+    }
+    
+    /**
+     * Replace ',' on '.' for js
+     *
+     * @param float $price
+     * @return string
+     */
+    protected function _registerJsPrice($price)
+    {
+        return str_replace(',', '.', $price);
+    }
+    
+    /**
+     * Convert price from default currency to current currency
+     *
+     * @param float $price
+     * @param boolean $round
+     * @return float
+     */
+    protected function _convertPrice($price, $round = false)
+    {
+        if (empty($price)) {
+            return 0;
+        }
+
+        $price = $this->_getStore()->convertPrice($price);
+        if ($round) {
+            $price = $this->_getStore()->roundPrice($price);
+        }
+
+        return $price;
     }
 }
