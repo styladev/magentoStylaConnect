622763ca9ef013d131ab7ba96522c2d21715d530
Justus Krapp <jk@ecocode.de>
2015-08-04 14:50:38 +0200

    SC-11 back to jquery :)

--- skin/frontend/base/default/js/styla/connect.js
+++ skin/frontend/base/default/js/styla/connect.js
@@ -1,7 +1,6 @@
-$(document).on('styla:cart:add_after', function(event) {
-    var eventData = event.memo || {};
+$j('document').on('styla_cart_add_after', function(eventData) {
     /**
-     * This script is waiting for an event "styla:cart:add_after" to fire.
+     * This script is waiting for an event "styla_cart_add_after" to fire.
      * The event is fired after a user successfully adds a new product to his cart, using the styla product story page.
      *
      * It's used to load updated minicart content and (possibly) open the minicart window, if possible.
@@ -11,17 +10,17 @@ $(document).on('styla:cart:add_after', function(event) {
     /**
      * Look for the default minicart block, and if it's found - update it's content with the data we got from this event
      */
-    var minicartContentElement = $("#header-cart");
-    if (minicartContentElement && eventData.hasOwnProperty('html')) {
-        minicartContentElement.update(eventData.html);
+    var $minicartContentElement = $j("#header-cart");
+    if ($minicartContentElement && eventData.hasOwnProperty('html')) {
+        $minicartContentElement.html(eventData.html);
     }
 
     /**
      * The default theme (rwd/default) also has a numeric label next to the minicart - we can update it using the raw
      * cart "metadata".
      */
-    var cartCountElement = $$(".skip-cart .count").first();
-    if (cartCountElement && eventData.hasOwnProperty('meta')) {
-        cartCountElement.update(eventData.meta.items_qty);
+    var $cartCountElement = $j(".skip-cart .count");
+    if ($cartCountElement && eventData.hasOwnProperty('meta')) {
+        $cartCountElement.html(eventData.meta.num_items);
     }
 });
