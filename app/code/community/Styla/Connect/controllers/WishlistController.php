69d5ff795d8fd7a984e021ef85e0034d3bca0bb5
Justus Krapp <jk@ecocode.de>
2015-12-17 13:58:36 +0100

    SC-15 small fix for wishlist and some refactoring of existing code

--- app/code/community/Styla/Connect/controllers/WishlistController.php
+++ app/code/community/Styla/Connect/controllers/WishlistController.php
@@ -1,6 +1,9 @@
 <?php
 require_once('Mage/Wishlist/controllers/IndexController.php');
 
+/**
+ * Class Styla_Connect_WishlistController
+ */
 class Styla_Connect_WishlistController extends Mage_Wishlist_IndexController
 {
     public function preDispatch()
@@ -8,37 +11,56 @@ class Styla_Connect_WishlistController extends Mage_Wishlist_IndexController
         //default preDispatch of the wishlist controller would redirect us to login page. we don't need that.
         return Mage_Core_Controller_Front_Action::preDispatch();
     }
-    
+
     public function addAction()
     {
-        //check if the customer is logged in, and the product requested is real
+        $helper = Mage::helper('wishlist');
+        if (!Mage::getStoreConfigFlag('wishlist/general/active')) {
+            //wishlist feature is not enabled -> 501 Not Implemented
+            return $this->setResponse(
+                501,
+                array('error' => $helper->__('Could not find wishlist'))
+            );
+        }
+
         $product = $this->_initProduct();
-        if(!$product || !Mage::getSingleton('customer/session')->isLoggedIn() || !Mage::getStoreConfigFlag('wishlist/general/active')) {
-            $this->_responseError();
-            return;
+        if (!$product) {
+            //product does not exists -> 404 not found
+            return $this->setResponse(
+                404,
+                array('error' => $helper->__('Cannot specify product.'))
+            );
         }
-        
-        if(!$this->_addProductToWishList($product)) {
-            $this->_responseError();
-            return;
+
+        if (!Mage::getSingleton('customer/session')->isLoggedIn()) {
+            //wishlist needs the user to be logged in -> 401 Unauthorized
+            return $this->setResponse(
+                401,
+                array('error' => $helper->__('You need be logged in to use the wishlist'))
+            );
         }
-        
-        //all good at this point, we may think about a return value
-        
-        $resultArray = array(
-            'success'   => true
-        );
-        
-        $this->getResponse()->setHeader('Content-type', 'application/json');
-        $this->getResponse()->setBody(json_encode($resultArray));
-        
-        return;
+
+        try {
+            if (!$this->_addProductToWishList($product)) {
+                return $this->setResponse(500, $helper->__('Cannot update wishlist'));
+            }
+
+            //all good at this point
+            return $this->setResponse(
+                200,
+                array('success' => true)
+            );
+        } catch (Exception $e) {
+            Mage::logException($e);
+            $this->setResponse(500, $helper->__('Cannot update wishlist'));
+        }
     }
-    
+
     /**
      * Add the item to wish list
      *
      * @param Mage_Catalog_Model_Product $product
+     * @return bool
      */
     protected function _addProductToWishList($product)
     {
@@ -47,58 +69,58 @@ class Styla_Connect_WishlistController extends Mage_Wishlist_IndexController
             return false;
         }
 
-        $session = Mage::getSingleton('customer/session');
-        $success = false;
+        $requestParams = $this->getRequest()->getParams();
+        $buyRequest    = new Varien_Object($requestParams);
 
-        try {
-            $requestParams = $this->getRequest()->getParams();
-            $buyRequest = new Varien_Object($requestParams);
-
-            $result = $wishlist->addNewItem($product, $buyRequest);
-            if (is_string($result)) {
-                Mage::throwException($result);
-            }
-            $wishlist->save();
-
-            Mage::dispatchEvent(
-                'wishlist_add_product',
-                array(
-                    'wishlist' => $wishlist,
-                    'product' => $product,
-                    'item' => $result
-                )
-            );
-
-            Mage::helper('wishlist')->calculate();
-
-            $success = true;
-        } catch (Mage_Core_Exception $e) {
-            return false;
+        $result = $wishlist->addNewItem($product, $buyRequest);
+        if (is_string($result)) {
+            Mage::throwException($result);
         }
-        catch (Exception $e) {
-            return false;
-        }
+        $wishlist->save();
 
-        return $success;
+        Mage::dispatchEvent(
+            'wishlist_add_product',
+            array(
+                'wishlist' => $wishlist,
+                'product'  => $product,
+                'item'     => $result,
+            )
+        );
+
+        Mage::helper('wishlist')->calculate();
     }
-    
-    protected function _responseError()
-    {
-        $this->getResponse()->setHeader('HTTP/1.0','404',true);
-    }
-    
+
     /**
-     * 
+     * @param       $statusCode
+     * @param array $body
+     * @return $this
+     */
+    protected function setResponse($statusCode, $body = array())
+    {
+        $this->getResponse()
+            ->setHeader('HTTP/1.0', $statusCode, true)
+            ->setHeader('Content-type', 'application/json');
+
+        if ($body) {
+            $this->getResponse()->setBody(json_encode($body));
+        }
+
+        return $this;
+    }
+
+    /**
+     *
      * @return boolean|Mage_Catalog_Model_Product
      */
     protected function _initProduct()
     {
         $productId = $this->getRequest()->getParam('product');
-        if(!$productId) {
+        if (!$productId) {
             return false;
         }
-        
+
         $product = Mage::getModel('catalog/product')->load($productId);
+
         return $product && $product->getId() ? $product : false;
     }
 }
