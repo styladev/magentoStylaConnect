0d1979868d8d1bd645a483daf2bd63e32377dd58
Patryk Kata <patrykkata@gmail.com>
2016-01-27 11:53:29 +0100

    SC-17: adding the Magazine navigation link to page main navigation menu

---
+++ app/code/community/Styla/Connect/Model/Observer.php
@@ -0,0 +1,25 @@
+<?php
+class Styla_Connect_Model_Observer
+{
+    public function addNavigationLink(Varien_Event_Observer $observer)
+    {
+        /** @var Styla_Connect_Helper_Config $configHelper */
+        $configHelper = Mage::helper('styla_connect/config');
+        
+        if(!$configHelper->isNavigationLinkEnabled()) {
+            return;
+        }
+        
+        $menu = $observer->getMenu();
+        $tree = $menu->getTree(); 
+        
+        $magazineUrl = Mage::getBaseUrl() . $configHelper->getRouteName();
+        $magazineMenuNode = new Varien_Data_Tree_Node(array(
+            'name'   => $configHelper->getNavigationLinkLabel(),
+            'id'     => 'styla-magazine',
+            'url'    => $magazineUrl,
+        ), 'id', $tree, $menu);
+
+        $menu->addChild($magazineMenuNode);
+    }
+}
