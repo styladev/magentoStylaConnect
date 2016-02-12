de4ef1603892f65731b6177076695732af3ff755
Patryk Kata <patrykkata@gmail.com>
2016-02-12 12:29:54 +0100

    SC-18: adding installer for copying prod configuration for the new version

---
+++ app/code/community/Styla/Connect/data/styla_connect_setup/data-upgrade-0.1.1.4-0.1.1.5.php
@@ -0,0 +1,25 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+/**
+ * if the store was previously set to the production mode, we'll need to
+ * copy the configuration values to the new _prod paths
+ * 
+ */
+$configHelper = Mage::helper('styla_connect/config');
+$currentlyConfiguredFor = Mage::getStoreConfig('styla_connect/basic/mode');
+
+if($currentlyConfiguredFor == "prod") {
+    $configuration = $configHelper->getConfiguration();
+    
+    //there's a current configuration for prod enabled, so we'll need to fix the paths for it
+    foreach($configHelper->getApiConfigurationFields() as $fieldName => $field) {
+        $fieldValue = Mage::getStoreConfig($field);
+        if($fieldValue) {
+            $configuration->saveConfig($configHelper->getApiConfigurationFieldByMode($field, 'prod', true), $fieldValue);
+        }
+    }
+}
+
+$installer->endSetup();
