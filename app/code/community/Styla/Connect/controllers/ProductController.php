567d34c030797344c12e2b0061125a8be1040723
Patryk Kata <patrykkata@gmail.com>
2016-02-19 16:08:54 +0100

    SC-21: adding more meaningful error responses to product info calls

--- app/code/community/Styla/Connect/controllers/ProductController.php
+++ app/code/community/Styla/Connect/controllers/ProductController.php
@@ -12,24 +12,28 @@ class Styla_Connect_ProductController extends Mage_Core_Controller_Front_Action
      */
     public function infoAction()
     {
-        $product = $this->_initProduct();
-        if (!$product) {
-            $this->getResponse()->setHeader('HTTP/1.0', '404', true);
-
-            return;
-        }
-
         try {
+            $product = $this->_initProduct();
+            
             $productInfo = $this->_getProductInfo();
             $productInfo->setProduct($product);
 
             $this->getResponse()->setHeader('Content-type', 'application/json');
             $this->getResponse()->setBody(json_encode($productInfo->getInfo()));
 
+        } catch(Styla_Connect_Exception $e) {
+            //on known and recoverable exceptions, we're trying to generate a meaningful error response
+            $body = array("error" => $this->__($e->getMessage()), "saleable" => false);
+            
+            $this->getResponse()->setHeader('Content-type', 'application/json');
+            $this->getResponse()->setBody(json_encode($body));
+            
+            return;
+            
         } catch (Exception $e) {
             Mage::logException($e);
 
-            $this->getResponse()->setHeader('HTTP/1.0', '404', true);
+            $this->getResponse()->setHeader('HTTP/1.0', '500', true);
 
             return;
         }
@@ -43,12 +47,19 @@ class Styla_Connect_ProductController extends Mage_Core_Controller_Front_Action
     {
         $productSku = $this->getRequest()->getParam('sku');
         if (!$productSku) {
-            return false;
+            throw new Exception("Missing SKU.");
         }
 
         $product = Mage::getModel('catalog/product')->loadByAttribute('sku', $productSku);
-
-        return $product && $product->getId() ? $product : false;
+        if(!$product || ($product && !$product->getId())) {
+            throw new Styla_Connect_Exception("The product was not found.");
+        }
+        
+        if(!$product->isSaleable()) {
+            throw new Styla_Connect_Exception("Product is unavailable.");
+        }
+        
+        return $product;
     }
 
     /**
