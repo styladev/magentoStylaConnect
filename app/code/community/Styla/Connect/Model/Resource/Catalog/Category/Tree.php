edf7284d02c7404ff005f191a4f4e1af5f012f57
j.krapp <jk@ecocode.de>
2017-04-07 10:36:18 +0200

    master code formatting

--- app/code/community/Styla/Connect/Model/Resource/Catalog/Category/Tree.php
+++ app/code/community/Styla/Connect/Model/Resource/Catalog/Category/Tree.php
@@ -1,10 +1,14 @@
 <?php
+
+/**
+ * Class Styla_Connect_Model_Resource_Catalog_Category_Tree
+ */
 class Styla_Connect_Model_Resource_Catalog_Category_Tree extends Mage_Catalog_Model_Resource_Category_Tree
 {
     /**
      * Load a category tree, with up to $maxLevel of children category levels.
      * If 1, then only loads the first level of children, etc.
-     * 
+     *
      * @param int $parentId
      * @param int $maxLevels
      * @return \Styla_Connect_Model_Resource_Catalog_Category_Tree|boolean
@@ -12,32 +16,35 @@ class Styla_Connect_Model_Resource_Catalog_Category_Tree extends Mage_Catalog_Mo
      */
     public function loadMaxChildrenLevels($parentId, $additionalAttributes = array(), $maxLevels = 0)
     {
-        if(!$parentId) {
+        if (!$parentId) {
             throw new Exception("Parent id for loading the tree is missing.");
         }
-        
+
         $levelField = $this->_conn->quoteIdentifier('level');
         $pathField  = $this->_conn->quoteIdentifier('path');
-        
+
         //we want the parent's level
-        $select = $this->_conn->select()
-                ->from($this->_table, array('level', 'path'))
-                ->where('entity_id = ?', $parentId);
+        $select     = $this->_conn->select()
+            ->from($this->_table, array('level', 'path'))
+            ->where('entity_id = ?', $parentId);
         $parentData = $this->_conn->fetchRow($select);
-        
+
         $collection = $this->_createCollectionDataSelect(true, $additionalAttributes);
-        
+
         //the categories we take may only be the children of our parent, up to the max level we're interested in
-        if($maxLevels) {
-            $collection->where("{$levelField} <= ?", $maxLevels + $parentData['level']); //desiredLevels + our current level we're on
-        }        
+        if ($maxLevels) {
+            $collection->where(
+                "{$levelField} <= ?",
+                $maxLevels + $parentData['level']
+            ); //desiredLevels + our current level we're on
+        }
         $collection->where("{$pathField} LIKE '{$parentData['path']}%'");
-        
+
         //add all parents of our category, as well
         //as they're needed for building the full tree, later on
-        $parentPath = $parentData['path'];
+        $parentPath   = $parentData['path'];
         $grandParents = explode("/", $parentPath);
-        if(!empty($grandParents)) {
+        if (!empty($grandParents)) {
             $collection->orWhere('e.entity_id in (?)', $grandParents);
         }
 
@@ -51,10 +58,10 @@ class Styla_Connect_Model_Resource_Catalog_Category_Tree extends Mage_Catalog_Mo
         foreach ($arrNodes as $key => $nodeInfo) {
             $pathToParent = explode('/', $nodeInfo[$this->_pathField]);
             array_pop($pathToParent);
-            $pathToParent = implode('/', $pathToParent);
+            $pathToParent                   = implode('/', $pathToParent);
             $childrenItems[$pathToParent][] = $nodeInfo;
         }
-        
+
         $this->addChildNodes($childrenItems, '', null);
         return $this;
     }
