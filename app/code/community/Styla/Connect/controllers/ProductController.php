56fb9693f3d029531e4a9806ba6c9e8cb255378c
j.krapp <jk@ecocode.de>
2017-08-09 11:30:12 +0200

    [release] fix add store parameter to product info endpoint

--- app/code/community/Styla/Connect/controllers/ProductController.php
+++ app/code/community/Styla/Connect/controllers/ProductController.php
@@ -5,6 +5,32 @@
  */
 class Styla_Connect_ProductController extends Mage_Core_Controller_Front_Action
 {
+    protected $initialEnvironmentInfo;
+
+    protected function setStore()
+    {
+        $currentStoreId = (int)Mage::app()->getStore()->getId();
+        $targetStore    = Mage::app()->getRequest()->getParam('store');
+
+        $store = Mage::app()->getStore($targetStore);
+
+        if ($store && $currentStoreId !== (int)$store->getId()) {
+            /** @var Mage_Core_Model_App_Emulation $appEmulation */
+            $appEmulation                 = Mage::getSingleton('core/app_emulation');
+            $this->initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation((int)$store->getId());
+        }
+    }
+
+    protected function resetStore()
+    {
+        if ($this->initialEnvironmentInfo) {
+            /** @var Mage_Core_Model_App_Emulation $appEmulation */
+            $appEmulation = Mage::getSingleton('core/app_emulation');
+            $appEmulation->stopEnvironmentEmulation($this->initialEnvironmentInfo);
+        }
+    }
+
+
     /**
      * Load the product's details and return them as a json
      * This is used in ajax calls from the magazine.
@@ -12,13 +38,7 @@ class Styla_Connect_ProductController extends Mage_Core_Controller_Front_Action
     public function infoAction()
     {
         try {
-            $currentStoreId = (int)Mage::app()->getStore()->getId();
-            $targetStoreId  = Mage::app()->getRequest()->getParam('store');
-
-            if ($targetStoreId && $currentStoreId !== (int)$targetStoreId) {
-                $appEmulation           = Mage::getSingleton('core/app_emulation');
-                $initialEnvironmentInfo = $appEmulation->startEnvironmentEmulation((int) $targetStoreId);
-            }
+            $this->setStore();
 
             $product = $this->_initProduct();
 
@@ -28,18 +48,16 @@ class Styla_Connect_ProductController extends Mage_Core_Controller_Front_Action
             $this->getResponse()->setHeader('Content-type', 'application/json');
             $this->getResponse()->setBody(json_encode($productInfo->getInfo()));
 
-            if (isset($appEmulation, $initialEnvironmentInfo)) {
-                $appEmulation->stopEnvironmentEmulation($initialEnvironmentInfo);
-            }
-        } catch(Styla_Connect_Exception $e) {
+            $this->resetStore();
+        } catch (Styla_Connect_Exception $e) {
             //on known and recoverable exceptions, we're trying to generate a meaningful error response
             $body = array("error" => $this->__($e->getMessage()), "saleable" => false);
-            
+
             $this->getResponse()->setHeader('Content-type', 'application/json');
             $this->getResponse()->setBody(json_encode($body));
-            
+
             return;
-            
+
         } catch (Exception $e) {
             Mage::logException($e);
 
@@ -62,14 +80,14 @@ class Styla_Connect_ProductController extends Mage_Core_Controller_Front_Action
         }
 
         $product = Mage::getModel('catalog/product')->loadByAttribute('sku', $productSku);
-        if(!$product || ($product && !$product->getId())) {
+        if (!$product || ($product && !$product->getId())) {
             throw new Styla_Connect_Exception("The product was not found.");
         }
-        
-        if(!$product->isSaleable()) {
+
+        if (!$product->isSaleable()) {
             throw new Styla_Connect_Exception("Product is unavailable.");
         }
-        
+
         return $product;
     }
 
