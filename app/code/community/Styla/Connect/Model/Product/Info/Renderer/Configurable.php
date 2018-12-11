393cc4fcbc0c68595e88007070f8ca29a33300df
Enzo Perrotta <ep@kendesign.it>
2018-12-11 17:10:16 +0100

    Fixed a method compatibility warning

--- app/code/community/Styla/Connect/Model/Product/Info/Renderer/Configurable.php
+++ app/code/community/Styla/Connect/Model/Product/Info/Renderer/Configurable.php
@@ -98,7 +98,7 @@ class Styla_Connect_Model_Product_Info_Renderer_Configurable
      * @param array                      $productInfo
      * @return array
      */
-    protected function _collectAdditionalProductInfo($product, $productInfo)
+    protected function _collectAdditionalProductInfo(Mage_Catalog_Model_Product $product, $productInfo)
     {
         parent::_collectAdditionalProductInfo($product, $productInfo);
 
